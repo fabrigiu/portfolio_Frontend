6537f25fc5b9b61abf43de9a3127ad3e341d291f
fabrigiu <fabriciogiudici97@hotmail.com>
2022-10-31 00:33:57 -0300

    login/logout

--- portfolio_Frontend/src/app/app.module.ts
+++ portfolio_Frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,9 @@ import { LoginComponent } from './components/login/login.component';
     HttpClientModule
   ],
 
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
